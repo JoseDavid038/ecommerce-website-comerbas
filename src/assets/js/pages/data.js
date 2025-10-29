ff5c50bd06d4c200ae999ae2a0bdab7d8feda2ce
Jose David Gonzalez <programacionjose2@gmail.com>
2025-10-28 22:55:28 -0500

    split field with data and functions

---
+++ src/assets/js/pages/data.js
@@ -0,0 +1,113 @@
+export const productsMasVendidos =[{
+  image: './src/assets/images/products/TV CHALLENGER 50 Pulgadas 126.png',
+  name: 'TV CHALLENGER 50" Pulgadas 126',
+  price: 1449900
+},{
+  image: './src/assets/images/products/Nevera No Frost CHALLENGER 231 Litros.png',
+  name: 'Nevera No Frost CHALLENGER 231 Litros',
+  price: 1636900
+},{
+  image: './src/assets/images/products/TV CHALLENGER 40 Pulgadas 101 cm.png',
+  name: 'TV CHALLENGER 40" Pulgadas 101 cm',
+  price: 899900
+},{
+  image: './src/assets/images/products/Lavadora CHALLENGER.png',
+  name: 'Lavadora CHALLENGER Carga Superior 13 Kilos',
+  price: 1419900
+},{
+  image: './src/assets/images/products/TV CHALLENGER 40 Pulgadas 101 cm.png',
+  name: 'TV CHALLENGER 40" Pulgadas 101 cm',
+  price: 899900
+},{
+  image: './src/assets/images/products/Lavadora CHALLENGER.png',
+  name: 'Lavadora CHALLENGER Carga Superior 13 Kilos',
+  price: 1449900
+}];
+
+export const productsRecomedados1 =[{
+  image: './src/assets/images/products/Nevera No Frost CHALLENGER 231 Litros.png',
+  name: 'Nevera No Frost CHALLENGER 231 Litros',
+  price: 1636900
+},{
+  image: './src/assets/images/products/TV CHALLENGER 50 Pulgadas 126.png',
+  name: 'TV CHALLENGER 50" Pulgadas 126',
+  price: 1449900
+},{
+  image: './src/assets/images/products/TV CHALLENGER 40 Pulgadas 101 cm.png',
+  name: 'TV CHALLENGER 40" Pulgadas 101 cm',
+  price: 899900
+},{
+  image: './src/assets/images/products/Lavadora CHALLENGER.png',
+  name: 'Lavadora CHALLENGER Carga Superior 13 Kilos',
+  price: 1419900
+}];
+
+export const productsRecomedados2 =[{
+  image: './src/assets/images/products/Nevera No Frost CHALLENGER 231 Litros.png',
+  name: 'Nevera No Frost CHALLENGER 231 Litros',
+  price: 1636900
+},{
+  image: './src/assets/images/products/TV CHALLENGER 50 Pulgadas 126.png',
+  name: 'TV CHALLENGER 50" Pulgadas 126',
+  price: 1449900
+},{
+  image: './src/assets/images/products/TV CHALLENGER 40 Pulgadas 101 cm.png',
+  name: 'TV CHALLENGER 40" Pulgadas 101 cm',
+  price: 899900
+},{
+  image: './src/assets/images/products/Lavadora CHALLENGER.png',
+  name: 'Lavadora CHALLENGER Carga Superior 13 Kilos',
+  price: 1419900
+}];
+
+
+
+export const audio =[{
+  image: './src/assets/images/products/mini componente Lg.png',
+  name: 'Minicomponente LG XBOOM RNC9 1800 Watts Negro Torre de Sonido',
+  price: 1349900
+},{
+  image: './src/assets/images/products/mini componente samsung.png',
+  name: 'Minicomponente SAMSUNG MX-T70 1500 Watts Negro Torre de Sonido',
+  price: 1636900
+}];
+
+export const celulares =[{
+  image: './src/assets/images/products/celular motorola.png',
+  name: 'Celular MOTOROLA Edge 60 Fusion 256GB 5G Verde',
+  price: 1349900
+},{
+  image: './src/assets/images/products/iphone.png',
+  name: 'iPhone 13 128GB Blanco Estrella',
+  price: 1636900
+}];
+
+export const lavadoras =[{
+  image: './src/assets/images/products/lavadora whirpool.png',
+  name: 'Lavadora WHIRLPOOL Carga Superior 20 Kilos WW20LTAHLA Smart Action Gris',
+  price: 1349900
+},{
+  image: './src/assets/images/products/Lavadora CHALLENGER.png',
+  name: 'Lavadora CHALLENGER Carga Superior 13 Kilos',
+  price: 1636900
+}];
+
+export const cocina =[{
+  image: './src/assets/images/products/estufa haceb.png',
+  name: 'Estufa de Piso HACEB 4 Puestos Con Horno Gas Natural Romero 50-Torre Negro',
+  price: 1349900
+},{
+  image: './src/assets/images/products/estufa abba.png',
+  name: 'Estufa de Piso HACEB 4 Puestos Con Horno Gas Natural Romero 50-Torre Negro',
+  price: 1636900
+}];
+
+export const televisores =[{
+  image: './src/assets/images/products/TV CHALLENGER 40 Pulgadas 101 cm.png',
+  name: 'TV CHALLENGER 40" Pulgadas 101 cm',
+  price: 1349900
+},{
+  image: './src/assets/images/products/TV CHALLENGER 50 Pulgadas 126.png',
+  name: 'TV CHALLENGER 50" Pulgadas 126',
+  price: 1636900
+}];
--- src/assets/js/pages/products-grid.js
+++ src/assets/js/pages/products-grid.js
@@ -1,53 +1,5 @@
 
-const audio =[{
-  image: './src/assets/images/products/mini componente Lg.png',
-  name: 'Minicomponente LG XBOOM RNC9 1800 Watts Negro Torre de Sonido',
-  price: 1349900
-},{
-  image: './src/assets/images/products/mini componente samsung.png',
-  name: 'Minicomponente SAMSUNG MX-T70 1500 Watts Negro Torre de Sonido',
-  price: 1636900
-}];
-
-const celulares =[{
-  image: './src/assets/images/products/celular motorola.png',
-  name: 'Celular MOTOROLA Edge 60 Fusion 256GB 5G Verde',
-  price: 1349900
-},{
-  image: './src/assets/images/products/iphone.png',
-  name: 'iPhone 13 128GB Blanco Estrella',
-  price: 1636900
-}];
-
-const lavadoras =[{
-  image: './src/assets/images/products/lavadora whirpool.png',
-  name: 'Lavadora WHIRLPOOL Carga Superior 20 Kilos WW20LTAHLA Smart Action Gris',
-  price: 1349900
-},{
-  image: './src/assets/images/products/Lavadora CHALLENGER.png',
-  name: 'Lavadora CHALLENGER Carga Superior 13 Kilos',
-  price: 1636900
-}];
-
-const cocina =[{
-  image: './src/assets/images/products/estufa haceb.png',
-  name: 'Estufa de Piso HACEB 4 Puestos Con Horno Gas Natural Romero 50-Torre Negro',
-  price: 1349900
-},{
-  image: './src/assets/images/products/estufa abba.png',
-  name: 'Estufa de Piso HACEB 4 Puestos Con Horno Gas Natural Romero 50-Torre Negro',
-  price: 1636900
-}];
-
-const televisores =[{
-  image: './src/assets/images/products/TV CHALLENGER 40 Pulgadas 101 cm.png',
-  name: 'TV CHALLENGER 40" Pulgadas 101 cm',
-  price: 1349900
-},{
-  image: './src/assets/images/products/TV CHALLENGER 50 Pulgadas 126.png',
-  name: 'TV CHALLENGER 50" Pulgadas 126',
-  price: 1636900
-}];
+import { audio,celulares,lavadoras,cocina,televisores } from './data.js';
 
 // formateador de moneda
 const formatoCOP = new Intl.NumberFormat('es-CO', {
