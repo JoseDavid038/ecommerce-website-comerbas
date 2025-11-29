40ac57be9e68af16c770939e6c80a18dd92c0f47
Jose David Gonzalez <programacionjose2@gmail.com>
2025-11-28 22:40:45 -0500

    fix bug in github pages, update routes

--- src/assets/js/pages/modal.js
+++ src/assets/js/pages/modal.js
@@ -1,5 +1,5 @@
 // Cargar modal desde /components/modal.html
-fetch("/components/modal.html")
+fetch("./components/modal.html")
   .then(res => res.text())
   .then(html => {
     document.body.insertAdjacentHTML("beforeend", html);
